2630dfc1453791de29a030a849017ba4d95ab3b1
Anti Mấy Thằng Loz dec tool ở bố <133343594+thuanantimaythangdectool@users.noreply.github.com>
2024-08-07 05:09:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+
+# Hàm tạo chuỗi ngẫu nhiên
 random() {
   tr </dev/urandom -dc A-Za-z0-9 | head -c5
   echo
 }
 
+# Mảng để tạo IP ngẫu nhiên
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+# Hàm tạo địa chỉ IP v6 ngẫu nhiên
 gen64() {
   ip64() {
     echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -12,20 +17,7 @@ gen64() {
   echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
-install_3proxy() {
-  echo "installing 3proxy"
-  URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-  wget -qO- $URL | bsdtar -xvf-
-  cd 3proxy-3proxy-0.8.6
-  make -f Makefile.Linux
-  mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-  cp src/3proxy /usr/local/etc/3proxy/bin/
-  cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-  chmod +x /etc/init.d/3proxy
-  chkconfig 3proxy on
-  cd $WORKDIR
-}
-
+# Hàm tạo cấu hình 3proxy
 gen_3proxy() {
   cat <<EOF
 daemon
@@ -46,12 +38,14 @@ $(awk -F "/" '{print "auth strong\n" \
 EOF
 }
 
+# Hàm tạo tệp proxy cho người dùng
 gen_proxy_file_for_user() {
   cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
+# Hàm upload proxy và tạo liên kết tải về
 upload_proxy() {
   local PASS=$(random)
   zip --password $PASS proxy.zip proxy.txt
@@ -62,12 +56,14 @@ upload_proxy() {
   echo "Password: ${PASS}"
 }
 
+# Hàm cài đặt jq
 install_jq() {
-  wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+  wget -O jq https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64
   chmod +x ./jq
   cp jq /usr/bin
 }
 
+# Hàm upload tệp với password bảo vệ
 upload_2file() {
   local PASS=$(random)
   zip --password $PASS proxy.zip proxy.txt
@@ -79,44 +75,46 @@ upload_2file() {
   echo "Password: ${PASS}"
 }
 
+# Hàm tạo dữ liệu cho proxy
 gen_data() {
   seq $FIRST_PORT $LAST_PORT | while read port; do
     echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
   done
 }
 
+# Hàm tạo lệnh iptables
 gen_iptables() {
   cat <<EOF
     $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
+# Hàm tạo lệnh ifconfig
 gen_ifconfig() {
   cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
-echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
+# Cài đặt các ứng dụng cần thiết
+echo "Installing apps..."
+yum -y install gcc net-tools bsdtar zip jq >/dev/null
 
-install_3proxy
-
-echo "working folder = /home/proxy-installer"
+# Thiết lập thư mục làm việc và các biến môi trường
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir -p $WORKDIR && cd $_ || exit
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
+echo "How many proxies do you want to create? Example 500"
+read -r COUNT
 
 FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+LAST_PORT=$(($FIRST_PORT + $COUNT - 1))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
@@ -125,17 +123,20 @@ chmod +x boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
+# Tạo tệp rc.local để khởi động các dịch vụ và cấu hình
 cat > /etc/rc.local <<EOF
+#!/bin/bash
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
-/usr/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
+/usr/local/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
 
+chmod +x /etc/rc.local
 bash /etc/rc.local
 
+# Tạo tệp proxy cho người dùng và upload
 gen_proxy_file_for_user
 
-# upload_proxy
-
+# Upload proxy và cài đặt jq
 install_jq && upload_2file
